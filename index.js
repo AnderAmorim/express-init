1f9fddca53f3838fd866203d9c6bb02fbbef92b7
Anderson <andersonamorim939@gmail.com>
2020-06-04 21:24:46 -0300

    Create routers

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const app = express();
+
+app.get('/',(req,res)=>{
+  res.send('My first router')
+})
+
+app.get("/blog",(req,res)=>{
+  res.send("Welcome to my blog")
+})
+
 const port = 4000;
 app.listen(port,function(erro){
   if(erro){
